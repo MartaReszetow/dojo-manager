06e186d5803c8f5d8cb4fae6e8bee586e50c1280
MARTA RESZETOW <marta.reszetow@gmail.com>
2021-08-07 10:37:31 +0200

    creating new project to manage payments inside dojo

---
+++ src/main/java/com/sda/spring/dojoManager/DojoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sda.spring.dojoManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/spring/dojoManager/DojoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.spring.dojoManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
